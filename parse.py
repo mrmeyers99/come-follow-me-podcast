7752539bb894a2f43d280ad7fdd7a281a5fb6bdb
Michael Meyers <mmeyers@bandwidth.com>
2019-05-22 13:23:35 -0400

    fix deployment

--- parse.py
+++ parse.py
@@ -31,11 +31,11 @@ print(start_date, '-', end_date)
 print("""<?xml version="1.0" encoding="utf-8"?>
 <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:itunesu="http://www.itunesu.com/feed" version="2.0">
 <channel>
-<link>http://www.YourSite.com</link>
+<link>https://www.lds.org</link>
 <language>en-us</language>
-<copyright>&#xA9;2013</copyright>
-<webMaster>your@email.com (Your Name)</webMaster>
-<managingEditor>your@email.com (Your Name)</managingEditor>
+<copyright>&#xA9;2019</copyright>
+<webMaster>mrmeyers99@gmail.com (Michael)</webMaster>
+<managingEditor>mrmeyers99@gmail.com (Michael)</managingEditor>
 <image>
 <url>http://www.YourSite.com/ImageSize300X300.jpg</url>
 <title>Title or description of your logo</title>
@@ -45,20 +45,18 @@ print("""<?xml version="1.0" encoding="utf-8"?>
 <itunes:name>Michael Meyers</itunes:name>
 <itunes:email>mrmeyers99@gmail.com</itunes:email>
 </itunes:owner>
-<itunes:category text="Education">
-<itunes:category text="Higher Education" />
+<itunes:category text="Religion">
+<itunes:category text="Spirituality" />
 </itunes:category>
-<itunes:keywords>separate, by, comma, and, space</itunes:keywords>
+<itunes:keywords>come, follow, me</itunes:keywords>
 <itunes:explicit>no</itunes:explicit>
-<itunes:image href="http://www.YourSite.com/ImageSize300X300.jpg" />
+<itunes:image href="https://content.ldschurch.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/2018/400x640/slctemple2.jpg" />
 <atom:link href="http://www.YourSite.com/feed.xml" rel="self" type="application/rss+xml" />
-<pubDate>Sun, 01 Jan 2012 00:00:00 EST</pubDate>
-<title>Verbose title of the podcast</title>
-<itunes:author>College, school, or department owning the podcast</itunes:author>
-<description>Verbose description of the podcast.</description>
-<itunes:summary>Duplicate of above verbose description.</itunes:summary>
-<itunes:subtitle>Short description of the podcast - 255 character max.</itunes:subtitle>
-<lastBuildDate>Thu, 02 Feb 2012 00:00:00 EST</lastBuildDate>""")
+<title>Come Follow Me Scriptures</title>
+<itunes:author></itunes:author>
+<description></description>
+<itunes:summary></itunes:summary>
+<itunes:subtitle></itunes:subtitle>""")
 
 
 for chapter in chapters:
@@ -67,13 +65,12 @@ for chapter in chapters:
     print("""
 <item>
   <title>""" + title + ' - ' + chapter + """</title>
-  <description>Verbose description of the episode.</description>
+  <description>""" + title + ' - ' + chapter + """</description>
   <itunes:summary></itunes:summary>
   <itunes:subtitle></itunes:subtitle>
   <itunesu:category itunesu:code="112" />
-  <enclosure url=\"""" + url + """\" type="audio/mpeg" length="1" />
+  <enclosure url=\"""" + url + """\" type="audio/mpeg" />
   <guid>""" + url + """</guid>
-  <itunes:duration>00:00:00</itunes:duration>
   <pubDate>""" + start_date.strftime('%a, %d %b %Y %H:%M:%S EST') + """</pubDate>
 </item>""")
 
